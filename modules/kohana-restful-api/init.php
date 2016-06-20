5e9baab1230a14b04d66186622f5879680c1b015
killer-djon <kil-djon@yandex.ru>
2016-06-20 15:00:34 +0300

    Remove submodule modules/kohana-restful-api

---
+++ modules/kohana-restful-api/init.php
@@ -0,0 +1,22 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+if( !Route::cache() )
+{
+    Route::set('articles', 'api/v<version>/<format>(/<item_per_page>(/<page>))/<controller>(/<id>)',
+		array(
+			'version' => '[\d+]',
+			'format'  => '(json|xml|csv|html)',
+			'id'	=> '.*',
+			'item_per_page'	=> '[\d]+',
+			'page'	=> '[\d]+'
+		))
+		->defaults(array(
+			'format' => 'json',
+			'directory'	=> 'api'
+		));
+
+    if( Kohana::$caching === TRUE )
+    {
+        Route::cache(TRUE);
+    }
+}
